3575c6b9f47e0b4d37bb4a3523f6d2602ddf321b
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-01-26 17:26:40 +0530

    More tests
    The polygon I have is actually a pathological case, so it may not make sense.  A lot of center points of cells come on the edge, so keys are not unique.  I'm thinking of adding a minimum jitter so that keys in the priority queue remain more or less unique.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,44 @@
 using Polylabel
+import ArchGDAL, GeometryBasics, LibGEOS
+using GeoMakie
 using Test
 
 @testset "Polylabel.jl" begin
-    # Write your tests here.
-    @testset "Simple MWE" begin
-        p1 = Polylabel.GeometryBasics.Polygon([
-            Point2f(-1, 0),
-            Point2f(0, 1),
-            Point2f(2, 0),
-            Point2f(0, -1),
-            Point2f(-1, 0)
+    p1 = GeometryBasics.Polygon([
+            Point2{Float64}(-10, 0),
+            Point2{Float64}(0, 10),
+            Point2{Float64}(20, 0),
+            Point2{Float64}(0, -10),
+            Point2{Float64}(-10, 0)
         ])
-        labelpoint = Polylabel.polylabel(p1, tolerance = 0.05)
+
+    @testset "Signed distance" begin
+        @test Polylabel.signed_distance(p1, 0, 0) ≈ 10.0
+        @test Polylabel.signed_distance(p1, 30, 0) ≈ -10.0
     end
+
+    @testset "Cell construction" begin
+        c1 = Polylabel.Cell(GeoInterface.centroid(p1), 0, p1)
+        @test c1.max_distance ≈ 10.540925533894598
+        @test c1.distance ≈ 10.540925533894598
+        @test c1.x == GeoInterface.x(GeoInterface.centroid(p1))
+        @test c1.y == GeoInterface.y(GeoInterface.centroid(p1))
+    end
+
+    @testset "GeometryBasics MWE" begin
+        labelpoint = Polylabel.polylabel(p1, rtol = 0.1)
+        @test all(labelpoint .≈ (10/3, 0.0))
+    end
+    @testset "ArchGDAL MWE" begin
+        # define this method for now
+        Polylabel.GeoInterface.centroid(::Polylabel.GeoInterface.AbstractTrait, geom::ArchGDAL.IGeometry) = ArchGDAL.centroid(geom)
+        labelpoint = Polylabel.polylabel(Polylabel.GeoInterface.convert(ArchGDAL, p1), rtol = 0.1)
+        @test all(labelpoint .≈ (10/3, 0.0))
+    end
+    # @testset "LibGEOS MWE" begin
+    #     # define this method for now
+    #     Polylabel.GeoInterface.centroid(::Polylabel.GeoInterface.AbstractTrait, geom::LibGEOS.AbstractGeometry) = LibGEOS.centroid(geom)
+    #     labelpoint = Polylabel.polylabel(LibGEOS.Polygon(Polylabel.GeoInterface.coordinates(p1)), rtol = 0.1)
+    #     @test all(labelpoint .≈ (10/3, 0.0))
+    # end
 end
